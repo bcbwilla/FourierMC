0934b7f9ae592e4622034212b2101c2525cd3b70
ben <bcbwilla@gmail.com>
2015-01-22 21:31:30 -0500

    Pass criteria in through evaluate method

--- src/main/java/xyz/benw/plugins/fouriermc/DataAnalysis/DataTests/IDataTest.java
+++ src/main/java/xyz/benw/plugins/fouriermc/DataAnalysis/DataTests/IDataTest.java
@@ -4,10 +4,11 @@ package xyz.benw.plugins.fouriermc.DataAnalysis.DataTests;
  * IDataTest
  *
  * Interface for all tests on clicking data.
+ * A data test tests one specific thing.
  */
 public interface IDataTest {
 
     /* Evaluate the test */
-    public boolean evaluate();
+    public boolean evaluate(double critera);
 
 }
