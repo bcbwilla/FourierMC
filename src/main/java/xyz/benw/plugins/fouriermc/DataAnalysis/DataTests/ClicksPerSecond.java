068aa4a64c0594398928949ef3c704375bd6b94c
ben <bcbwilla@gmail.com>
2015-01-25 15:15:01 -0500

    refactor clicks per second

--- src/main/java/xyz/benw/plugins/fouriermc/DataAnalysis/DataTests/ClicksPerSecond.java
+++ src/main/java/xyz/benw/plugins/fouriermc/DataAnalysis/DataTests/ClicksPerSecond.java
@@ -10,9 +10,9 @@ import org.apache.commons.math3.stat.descriptive.summary.Sum;
 public class ClicksPerSecond implements IDataTest {
 
     private double[] data;
+    private double clicksPerSecond;
 
     private Long samplePeriod;
-    private double clicksPerSecond;
 
     /**
      * Class constructor.
@@ -23,10 +23,26 @@ public class ClicksPerSecond implements IDataTest {
     public ClicksPerSecond(double[] data, Long samplePeriod) {
         this.data = data;
         this.samplePeriod = samplePeriod;
+        this.clicksPerSecond = computeClicksPerSecond();
     }
 
     /**
-     * Computes clicks per second and compares to criteria.
+     * Computes the clicks per second
+     */
+    public double computeClicksPerSecond() {
+        // *20 to convert from ticks to seconds
+        int size = data.length;
+        double cps;
+        if(samplePeriod != 0 && size != 0) {
+            cps = (new Sum().evaluate(data) / (samplePeriod * size)) * 20;
+        } else {
+            cps = 0.0;
+        }
+        return cps;
+    }
+
+    /**
+     * Compares clicks per second to critera.
      *
      * @param criteria the value which is compared to the computed click speed. If computed value is larger,
      *                 the test is failed.
@@ -35,15 +51,7 @@ public class ClicksPerSecond implements IDataTest {
      */
     @Override
     public boolean evaluate(double criteria) {
-
-        // *20 to convert from ticks to seconds
-        int size = data.length;
-        if(samplePeriod != 0 && size != 0) {
-            clicksPerSecond = (new Sum().evaluate(data) / (samplePeriod * size)) * 20;
-        } else {
-            clicksPerSecond = 0.0;
-        }
-        return clicksPerSecond < criteria;
+        return getClicksPerSecond() < criteria;
     }
 
     /**
