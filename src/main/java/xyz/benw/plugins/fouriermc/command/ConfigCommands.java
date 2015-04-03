d0a722bf91f75c65c58b920646ac47816ddc69ef
ben <bcbwilla@gmail.com>
2015-04-02 20:03:31 -0400

    add more basic commands and permissions

--- src/main/java/xyz/benw/plugins/fouriermc/command/ConfigCommands.java
+++ src/main/java/xyz/benw/plugins/fouriermc/command/ConfigCommands.java
@@ -1,5 +1,6 @@
 package xyz.benw.plugins.fouriermc.command;
 
+import org.apache.commons.lang.StringUtils;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -9,6 +10,7 @@ import org.bukkit.configuration.file.FileConfiguration;
 import xyz.benw.plugins.fouriermc.FourierMC;
 
 import java.util.Map;
+import java.util.Set;
 
 /**
  * Basic config related commands
@@ -29,20 +31,35 @@ public class ConfigCommands implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         if (cmd.getName().equalsIgnoreCase("fmc")) {
 
-           if(args.length == 1 && args[0].equalsIgnoreCase("list")) {
-               Map<String, Object> configParams = config.getValues(true);
+            if (!sender.hasPermission("fmc")) {
+                sender.sendMessage(ChatColor.RED + "You don't have permission.");
+                return true;
+            }
 
-               sender.sendMessage(ChatColor.DARK_AQUA + "FourierMC Config Settings:");
-               for (Map.Entry<String, Object> entry : configParams.entrySet()) {
-                   String key = entry.getKey();
-                   Object value = entry.getValue();
+            if(args.length == 1 && args[0].equalsIgnoreCase("list")) {
+                Map<String, Object> configParams = config.getValues(true);
 
-                   if(!(value instanceof MemorySection)) {
-                       sender.sendMessage(" " + key + ": " + value.toString());
-                   }
-               }
-               return true;
-           }
+                sender.sendMessage(ChatColor.DARK_AQUA + "FourierMC Config Settings:");
+                for (Map.Entry<String, Object> entry : configParams.entrySet()) {
+                    String key = entry.getKey();
+                    Object value = entry.getValue();
+
+                    if(!(value instanceof MemorySection)) {
+                        sender.sendMessage(" " + key + ": " + value.toString());
+                    }
+                }
+                return true;
+
+            } else if(args.length == 2 && args[0].equalsIgnoreCase("get")) {
+                sender.sendMessage(args[1] + ": " + config.get(args[1]));
+                return true;
+
+            } else if (args.length == 1 && args[0].equalsIgnoreCase("reloadconfig")) {
+                this.plugin.reloadConfig();
+                sender.sendMessage("Reloaded config for FourierMC.");
+                return true;
+            }
+
         }
         return false;
     }
